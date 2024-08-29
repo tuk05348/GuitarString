d4e903d62e5d1432071119d74f85a90576ae00a9
Niaz Baharudeen <tuk05348@temple.edu>
2024-08-29 10:31:14 -0400

    Populated RingBuffer API with empty functions, created JUnit test file to test those functions.

--- src/main/java/work/niaz/example/GuitarString/RingBuffer.java
+++ src/main/java/work/niaz/example/GuitarString/RingBuffer.java
@@ -1,5 +1,34 @@
 package work.niaz.example.GuitarString;
 
 public class RingBuffer {
-
+	
+	private double[] ringBuffer;
+	
+	public RingBuffer(int capacity) {
+		this.ringBuffer = new double[capacity];
+	}
+	
+	int size() {
+		return 0;
+	}
+	
+	boolean isEmpty() {
+		return false;
+	}
+	
+	boolean isFull() {
+		return false;
+	}
+	
+	void enqueue(double x) {
+		
+	}
+	
+	double dequeue() {
+		return 0;
+	}
+	
+	double peek() {
+		return 0;
+	}
 }
