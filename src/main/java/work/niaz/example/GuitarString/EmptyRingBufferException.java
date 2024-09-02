203bb4e4e59f563b4a83221f281d879932a322b2
Niaz Baharudeen <tuk05348@temple.edu>
2024-09-02 13:52:42 -0400

    Created new custom exceptions for when one attempts to enqueue to a full buffer or when one attempts to dequeue an empty buffer.

---
+++ src/main/java/work/niaz/example/GuitarString/EmptyRingBufferException.java
@@ -0,0 +1,8 @@
+package work.niaz.example.GuitarString;
+
+public class EmptyRingBufferException extends Exception {
+	
+	public EmptyRingBufferException(String e) {
+		super(e);
+	}
+}
---
+++ src/main/java/work/niaz/example/GuitarString/FullRingBufferException.java
@@ -0,0 +1,8 @@
+package work.niaz.example.GuitarString;
+
+public class FullRingBufferException extends Exception {
+	
+	public FullRingBufferException(String e) {
+		super(e);
+	}
+}
