40eefdb8a98aa6149087211e311a3cd814b88dcd
Niaz Baharudeen <tuk05348@temple.edu>
2024-09-04 13:11:17 -0400

    Created an empty GuitarString API for simulating the behavior of a guitar string when plucked.

---
+++ src/main/java/work/niaz/example/GuitarString/GuitarString.java
@@ -0,0 +1,37 @@
+package work.niaz.example.GuitarString;
+
+/**
+ * Guitar String API
+ * Simulates the behavior of a guitar string when plucked by using the Ring Buffer API
+ */
+
+public class GuitarString {
+	
+	private int samplingRate = 44100;
+	private RingBuffer ringBuffer;
+	private int simTime;
+
+	public GuitarString(double frequency) {
+		
+	}
+	
+	public GuitarString(double[] init) {
+		
+	}
+	
+	void pluck() {
+		
+	}
+	
+	void tic() {
+		
+	}
+	
+	double sample() {
+		return 0;
+	}
+	
+	int time() {
+		return 0;
+	}
+}
