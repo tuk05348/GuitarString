72ae2ba24721c5b283b1724fbda1874ab48cdb21
Niaz Baharudeen <tuk05348@temple.edu>
2024-09-07 18:51:48 -0400

    Made mistake in order, wrote new test for size since we'll need size to fully test peek(). Watched test fail.

--- src/test/java/work/niaz/example/GuitarString/RingBufferTest.java
+++ src/test/java/work/niaz/example/GuitarString/RingBufferTest.java
@@ -68,6 +68,12 @@ public class RingBufferTest
 	}
 	
 	@Test
+	public void testSizeOnNonEmptyBuffer() {
+		ringBuffer.enqueue(1);
+		assertEquals(1, ringBuffer.size(), 0.0001, "Size should be 1 after 1 enqueue.");
+	}
+	
+	@Test
 	public void testPeekOnNonEmptyBuffer() {
 		ringBuffer.enqueue(1);
 		assertEquals(1, ringBuffer.peek(), 0.0001, "Peek should return first element enqueued to buffer.");
