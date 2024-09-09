867269c0835308c5ea4affdd461832c9e3321b00
Niaz Baharudeen <tuk05348@temple.edu>
2024-09-08 23:44:06 -0400

    Added comments to guitar string.

--- src/main/java/work/niaz/example/GuitarString/GuitarString.java
+++ src/main/java/work/niaz/example/GuitarString/GuitarString.java
@@ -7,7 +7,7 @@ package work.niaz.example.GuitarString;
 
 public class GuitarString {
 	
-	private int samplingRate = 44100;
+	private int samplingRate = 44100; //sampling rate
 	private RingBuffer ringBuffer;
 	private int simTime;
 
@@ -23,26 +23,43 @@ public class GuitarString {
 		}
 	}
 	
+	/**
+	 * Randomize the ring buffer of the guitar string with random values between 0.5 and -0.5
+	 */
 	void pluck() {
-		for(int i=0; i<ringBuffer.size(); i++) {
+		for(int i=0; i<ringBuffer.size(); i++) { //replace all existing items with random values
 			ringBuffer.dequeue();
-			ringBuffer.enqueue(Math.ceil(Math.random() - 0.5));
+			ringBuffer.enqueue(Math.random() - 0.5);
 		}
-		while(!ringBuffer.isFull()) {
+		while(!ringBuffer.isFull()) { //if the buffer isn't full, fill in the rest of the buffer with random values
 			ringBuffer.enqueue(Math.ceil(Math.random() - 0.5));
 		}
 	}
 	
+	/**
+	 * Step the simulation one unit forward and add to the end of the buffer the average of the first two
+	 * samples multiplied by a decay factor of 0.994, dequeue the first sample.
+	 */
 	void tic() {
-		double item = ringBuffer.dequeue() + ringBuffer.peek();
-		ringBuffer.enqueue(item * 0.5 * 0.994);
-		simTime++;
+		double item = ringBuffer.dequeue() + ringBuffer.peek(); //order matters, dequeue first, then peek, to get first two samples
+		ringBuffer.enqueue(item * 0.5 * 0.994); //enqueue average of two samples multiplied by decay factory
+		simTime++; //step simTime
 	}
 	
+	/**
+	 * Return the first value in the ring buffer of the guitar string
+	 * 
+	 * @return the value at the head of the ring buffer
+	 */
 	double sample() {
 		return ringBuffer.peek();
 	}
 	
+	/**
+	 * Return how many times tic has been called
+	 * 
+	 * @return the number of times tic has been called
+	 */
 	int time() {
 		return simTime;
 	}
