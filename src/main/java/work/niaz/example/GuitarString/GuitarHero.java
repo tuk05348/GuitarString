9ff417b8926db499c6ddd695b2fec16635e6d89d
Niaz Baharudeen <tuk05348@temple.edu>
2024-09-11 11:42:55 -0400

    Completed guitar hero with support for 37 keys that each play their own note. Added a stddraw window for keyboard focus.

--- src/main/java/work/niaz/example/GuitarString/GuitarHero.java
+++ src/main/java/work/niaz/example/GuitarString/GuitarHero.java
@@ -1,9 +1,45 @@
 package work.niaz.example.GuitarString;
 
+import edu.princeton.cs.algs4.StdAudio;
+import edu.princeton.cs.algs4.StdDraw;
+
 public class GuitarHero {
 	
 	public static void main(String[] args) {
-		String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";		
+		String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
+		
+		StdDraw.setPenRadius(0.05);
+        StdDraw.setPenColor(StdDraw.BLUE);
+        StdDraw.point(0.5, 0.5);
+        StdDraw.setPenColor(StdDraw.MAGENTA);
+        StdDraw.line(0.2, 0.2, 0.8, 0.2);
+		
+		GuitarString[] guitarStrings = new GuitarString[37];
+		for(int i=0; i<keyboard.length(); i++) {
+			guitarStrings[i] = new GuitarString(calculateFreq(i));
+		}
+		
+		while(true) {
+			double sample = 0;
+			if (StdDraw.hasNextKeyTyped()) {
+                char key = StdDraw.nextKeyTyped();
+                int i = keyboard.indexOf(key);
+                if(i != -1) {
+                	System.out.println(i + " " + key);
+                	guitarStrings[i].pluck();
+                }
+            }
+			
+			for (GuitarString g : guitarStrings) {
+				sample += g.sample();
+			}
+			
+			StdAudio.play(sample);
+			
+			for (GuitarString g : guitarStrings) {
+				g.tic();
+			}
+		}
 	}
 	
 	public static double calculateFreq(int index) {
