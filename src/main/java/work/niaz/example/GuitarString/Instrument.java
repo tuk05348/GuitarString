e3c9da826b80574f9f8f5626c39e4b43eec7b5c1
Niaz Baharudeen <tuk05348@temple.edu>
2024-09-16 23:41:51 -0400

    Created a new interface for methods that an instrument should implement.

---
+++ src/main/java/work/niaz/example/GuitarString/Instrument.java
@@ -0,0 +1,12 @@
+package work.niaz.example.GuitarString;
+
+public interface Instrument {
+	
+	public void pluck();
+	
+	public void tic();
+	
+	public double sample();
+	
+	public double time();
+}
