2ed0fd2ba1b2aa9a6644bd921a0266de88612b41
Niaz Baharudeen <tuk05348@temple.edu>
2024-09-17 19:47:19 -0400

    Created two new classes implementing the instrument interface.

---
+++ src/main/java/work/niaz/example/GuitarString/Drum.java
@@ -0,0 +1,29 @@
+package work.niaz.example.GuitarString;
+
+public class Drum implements Instrument {
+
+	@Override
+	public void vibrate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void tic() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double sample() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int time() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
