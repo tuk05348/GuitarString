9047208f572d818ba2c4a3b0f71404e5f60ca336
Niaz Baharudeen <tuk05348@temple.edu>
2024-09-22 23:40:45 -0400

    Added new note file for playing. Discovered fatal error in polyphonic guitar player. The play method only works by playing samples one at a time. Need to create an audio file first to be able to use threads.

--- src/main/java/work/niaz/example/GuitarString/PolyphonicGuitarPlayer.java
+++ src/main/java/work/niaz/example/GuitarString/PolyphonicGuitarPlayer.java
@@ -5,25 +5,28 @@ package work.niaz.example.GuitarString;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 
  */
 public class PolyphonicGuitarPlayer {
 	
-	private double delay;
+	private long delay;
 	private int totalCount;
 	private String filename;
-	private ScheduledExecutorService schedulerExecutorService;
 	
-	public PolyphonicGuitarPlayer(double delay, int totalCount, String filename) {
+	public PolyphonicGuitarPlayer(long delay, int totalCount, String filename) {
 		this.delay = delay;
 		this.totalCount = totalCount;
 		this.filename = filename;
-		schedulerExecutorService = Executors.newScheduledThreadPool(totalCount);
 	}
 	
 	public void play() {
-		
+
+	}
+	public static void main(String[] args) {
+		PolyphonicGuitarPlayer polyphonicGuitarPlayer = new PolyphonicGuitarPlayer(8, 3, "frere.txt");
+		polyphonicGuitarPlayer.play();
 	}
 }
