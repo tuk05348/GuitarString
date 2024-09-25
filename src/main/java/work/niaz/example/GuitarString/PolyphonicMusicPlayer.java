10bb8dec3ca1276744a5a9e6349dda633bb379ed
Niaz Baharudeen <tuk05348@temple.edu>
2024-09-25 14:03:42 -0400

    Added support for playing homophonic music, playing a melody with a supporting harmony, by overloading the constructor so it can take two given audio files.

--- src/main/java/work/niaz/example/GuitarString/PolyphonicMusicPlayer.java
+++ src/main/java/work/niaz/example/GuitarString/PolyphonicMusicPlayer.java
@@ -16,26 +16,50 @@ public class PolyphonicMusicPlayer {
 	
 	private long delay;
 	private int totalCount;
-	private String audioFileString;
+	private String melodyFileString;
+	private String harmonyFileString;
+	private boolean polyCheck = false;
+	
 	
 	public PolyphonicMusicPlayer(long delay, int totalCount, String audioFileString) {
 		this.delay = delay;
 		this.totalCount = totalCount;
-		this.audioFileString = audioFileString;
+		this.melodyFileString = audioFileString;
+		this.polyCheck = true;
+	}
+	
+	public PolyphonicMusicPlayer(long delay, String melodyFileString, String harmonyFileString) {
+		this.delay = delay;
+		this.totalCount = 2;
+		this.melodyFileString = melodyFileString;
+		this.harmonyFileString = harmonyFileString;
 	}
 	
 	public void play() {
-		for(int i=0; i<totalCount; i++) {
-			StdAudio.playInBackground(audioFileString);
+		if(this.polyCheck) {
+			for(int i=0; i<totalCount; i++) {
+				StdAudio.playInBackground(melodyFileString);
+				try {
+					TimeUnit.SECONDS.sleep(delay);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		else {
+			StdAudio.playInBackground(melodyFileString);
 			try {
 				TimeUnit.SECONDS.sleep(delay);
 			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+			StdAudio.playInBackground(harmonyFileString);
 		}
+		
 	}
 	public static void main(String[] args) {
-		PolyphonicMusicPlayer polyphonicMusicPlayer = new PolyphonicMusicPlayer(8, 3, "frere.wav");
+		PolyphonicMusicPlayer polyphonicMusicPlayer = new PolyphonicMusicPlayer(0, "frere.wav", "stairway.wav");
 		polyphonicMusicPlayer.play();
 	}
 }
