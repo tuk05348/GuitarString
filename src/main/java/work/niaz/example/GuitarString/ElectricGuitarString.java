c779c2ea61fbe90f92c448b062c24a0e9b2cf8db
Niaz Baharudeen <tuk05348@temple.edu>
2024-09-30 13:05:21 -0400

    Added JavaDocs to incomplete Electric Guitar String.

--- src/main/java/work/niaz/example/GuitarString/ElectricGuitarString.java
+++ src/main/java/work/niaz/example/GuitarString/ElectricGuitarString.java
@@ -4,7 +4,8 @@
 package work.niaz.example.GuitarString;
 
 /**
- * 
+ * ElectricGuitarString
+ * Implements an Electric Guitar String by modifying the Karplus-Strong algorithm to account for distortion and feedback
  */
 public class ElectricGuitarString implements Instrument {
 
@@ -13,6 +14,11 @@ public class ElectricGuitarString implements Instrument {
 	private RingBuffer ringBuffer; //internal ring buffer
 	private int simTime; //internal simulation time tracker
 	
+	/**
+	 * Constructor to create an electric guitar at a specific frequency
+	 * 
+	 * @param frequency the frequency at which the electric guitar string plays at
+	 */
 	public ElectricGuitarString(double frequency) { //create a new GuitarString with a given frequency
 		//divide the sampling rate by frequency and round up to the nearest integer
 		ringBuffer = new RingBuffer((int) Math.ceil(((double) SAMPLING_RATE)/frequency));
@@ -21,6 +27,9 @@ public class ElectricGuitarString implements Instrument {
 		}
 	}
 	
+	/**
+	 * Randomize the contents of the internal ring buffer of the electric guitar string with values between -0.5 and 0.5
+	 */
 	@Override
 	public void vibrate() {
 		while(!ringBuffer.isEmpty()) { //replace all existing items with random values
@@ -30,34 +39,55 @@ public class ElectricGuitarString implements Instrument {
 			ringBuffer.enqueue(Math.random() - 0.5);
 		}		
 	}
-
+	
+	/**
+	 * Step the simulation one step forward and apply the Karplus Strong update, add the average of the first 
+	 * two samples to the end of the ring buffer, remove the first sample
+	 * Modify the update by distorting the added average
+	 */
 	@Override
 	public void tic() {
 		double item = ringBuffer.dequeue() + ringBuffer.peek(); //order matters, dequeue first, then peek, to get first two samples
 		item *= 0.5;
-		ringBuffer.enqueue(((1.97 * item) - distortion(item)) * DECAY_FACTOR); //enqueue average of two samples multiplied by decay factory
+		ringBuffer.enqueue(((1.97 * item) - distortion(item)) * DECAY_FACTOR); //enqueue average of two samples multiplied by decay factor
 		//System.out.println(ringBuffer.peek());
 		simTime++; //step simTime
 	}
-
+	
+	/**
+	 * Return the first value in the internal ring buffer of the electric guitar string
+	 * 
+	 * @return the value at the head of the internal ring buffer
+	 */
 	@Override
 	public double sample() {
 		return ringBuffer.peek();
 	}
-
+	
+	/**
+	 * Returns the current time in the electric guitar string simulation
+	 * 
+	 * @return simTime the current time in the simulation
+	 */
 	@Override
 	public int time() {
 		return simTime;
 	}
 	
+	/**
+	 * Distort a given sample using a soft clipping function provided by Charles Sullivan in his paper
+	 * 
+	 * @param sample the sample to distort
+	 * @return the distortion of the sample
+	 */
 	public double distortion(double sample) {
-		if(sample >= 0.5) {
+		if(sample >= 0.5) { //if greater than 0.5, return positive threshold
 			return 0.1;
 		}
-		else if((sample < 0.5) && (sample > -0.5)) {
+		else if((sample < 0.5) && (sample > -0.5)) { //if between 0.5 and -0.5, return sample minus sample to third power divided by 3
 			return sample - (Math.pow(sample, 3)/3);
 		}
-		else {
+		else { //if less than 0.5, return negative threshold
 			return -0.1;
 		}
 	}
