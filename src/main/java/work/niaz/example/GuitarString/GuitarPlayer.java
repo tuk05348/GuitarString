25fd664e4061863857bdc87989d93cd15291b330
Niaz Baharudeen <tuk05348@temple.edu>
2024-09-30 12:35:26 -0400

    Added JavaDoc comments to GuitarPlayer and Polyphonic Guitar Player

--- src/main/java/work/niaz/example/GuitarString/GuitarPlayer.java
+++ src/main/java/work/niaz/example/GuitarString/GuitarPlayer.java
@@ -8,87 +8,117 @@ import java.util.HashSet;
 
 import edu.princeton.cs.algs4.StdAudio;
 
+/**
+ * GuitarPlayer
+ * Plays a a text file of a sequence of notes with a 37 note Guitar built using the GuitarString API
+ */
 public class GuitarPlayer {
-	private Path path;
+	private Path path; //file path of music file
+	
+	/**
+	 * Constructor to create a GuitarPlayer that reads a file to play music
+	 * 
+	 * @param filename name of the .txt file to read from, should be in the root of the working directory of the project
+	 */
 	public GuitarPlayer(String filename) {
-		try {
+		try { //try to assign a path to the file in the working directory
 			path = Paths.get(System.getProperty("user.dir") + "/" + filename);
-		} catch (Exception e) {
+		} catch (Exception e) { //catch the exception
 			System.out.println(e.getMessage());
 		}
 	}
 	
+	/**
+	 * Play the sequence of notes in the file line by line
+	 */
 	public void play() {
-		try {
+		try { //play the note sequence line by line
 			Files.lines(path).forEachOrdered(s -> playLine(s));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 	
+	/**
+	 * PLays the sequence of notes from the file line by line and records the audio to a .wav file
+	 * 
+	 * @param audioFileString name of the .wav file to record the audio to
+	 */
 	public void playAndRecord(String audioFileString) {
-		StdAudio.startRecording();
-		try {
+		StdAudio.startRecording(); //start recording
+		try { //play note sequence line by line
 			Files.lines(path).forEachOrdered(s -> playLine(s));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		double[] audio = StdAudio.stopRecording();
-		StdAudio.save(audioFileString, audio);
+		double[] audio = StdAudio.stopRecording(); //stop recording and store audio samples in an array of double
+		StdAudio.save(audioFileString, audio); //use the array of audio samples to save the recording to a file 
 	}
 	
+	/**
+	 * Plays a string of a sequence of notes
+	 * 
+	 * @param line string of the current line of the text file containing sequence of notes to read from
+	 */
 	private void playLine(String line) {
-		int samplingRate = 44100;
-		String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
+		int samplingRate = 44100; //sampling rate to play it
+		String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' "; //string of characters mapped to notes
 		
-		Instrument[] guitarStrings = new Instrument[37];
-		for(int i=0; i<keyboard.length(); i++) {
+		Instrument[] guitarStrings = new Instrument[37]; //Create a 37 note instrument, in this case a guitar
+		for(int i=0; i<keyboard.length(); i++) { //use equal temperament to assign notes
 			guitarStrings[i] = new GuitarString(Tuning.calculateFreq(i));
 		}
-		HashSet<Instrument> played = new HashSet<>();
+		HashSet<Instrument> played = new HashSet<>(); //Keep a hashset of notes that were played so we don't have to tic all 37
 
-		for(int i=0; i<line.length(); i++) {
-			if(line.charAt(i) == 'F') {
-				samplingRate = 22050;
+		for(int i=0; i<line.length(); i++) { //Play each note character by character
+			if(line.charAt(i) == 'F') { //Check for the 'F' tag, fast tempo, decrease sampling rate
+				samplingRate = 11025;
 			}
-			else if(line.charAt(i) == 'S') {
+			else if(line.charAt(i) == 'S') { //Check for the 'S' tag, slow tempo, return sampling rate to standard
 				samplingRate = 44100;
 			}
-			else if(line.charAt(i) == 'D') {
-				for(int k=i+1; k<i+3; k++) {
-					guitarStrings[keyboard.indexOf(line.charAt(k))].vibrate();
-					played.add(guitarStrings[keyboard.indexOf(line.charAt(k))]);
+			else if(line.charAt(i) == 'D') { //Check for the 'D' tag, dyad, vibrate two notes at the same time
+				for(int k=i+1; k<i+3; k++) { //vibrate the next 2 notes
+					int key = keyboard.indexOf(line.charAt(k));
+					guitarStrings[key].vibrate();
+					played.add(guitarStrings[key]);
 				}
-				i += 2;
+				i += 2; //increment i by 2
 			}
-			else if(line.charAt(i) == 'C') {
-				for(int k=i+1; k<i+4; k++) {
-					guitarStrings[keyboard.indexOf(line.charAt(k))].vibrate();
-					played.add(guitarStrings[keyboard.indexOf(line.charAt(k))]);
+			else if(line.charAt(i) == 'C') { //Check for the 'C' tag, chord, vibrate three notes at the same time
+				for(int k=i+1; k<i+4; k++) { //vibrate the next 3 notes
+					int key = keyboard.indexOf(line.charAt(k));
+					guitarStrings[key].vibrate();
+					played.add(guitarStrings[key]);
 				}
-				i += 3;
+				i += 3; //increment i by 3
 			}
-			else {
-				int key = keyboard.indexOf(line.charAt(i));
-				guitarStrings[key].vibrate();
-				played.add(guitarStrings[key]);
+			else { //Vibrate a single note
+				int key = keyboard.indexOf(line.charAt(i)); //get the note that was played from the keyboard mapping
+				guitarStrings[key].vibrate(); //vibrate the note
+				played.add(guitarStrings[key]); //add it to the set of played notes
 			}
 			
-			for(int j = 0; j < samplingRate; j++) {
+			for(int j = 0; j < samplingRate; j++) { //play each sample for 441000 iterations
 				double sample = 0;
-				for (Instrument instrument : played) {
+				for (Instrument instrument : played) { //for each played note add up the samples to get the superposition
 					sample += instrument.sample();
 				}
-				StdAudio.play(sample);
-				for (Instrument instrument : played) {
+				StdAudio.play(sample); //play the sample
+				for (Instrument instrument : played) { //tic each played instrument forward step
 					instrument.tic();
 				}
 			}
 		}
 	}
 	
+	/**
+	 * Test the guitar player by playing a text file of notes from the root of the working directory
+	 * 
+	 * @param args command line arguments
+	 */
 	public static void main(String[] args) {
-		GuitarPlayer guitarPlayer = new GuitarPlayer("frere.txt");
+		GuitarPlayer guitarPlayer = new GuitarPlayer("star.txt"); //this plays "Twinkle Twinkle Little Star"
 		guitarPlayer.play();
 	}
 }
--- src/main/java/work/niaz/example/GuitarString/PolyphonicMusicPlayer.java
+++ src/main/java/work/niaz/example/GuitarString/PolyphonicMusicPlayer.java
@@ -1,65 +1,82 @@
-/**
- * 
- */
 package work.niaz.example.GuitarString;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 import edu.princeton.cs.algs4.StdAudio;
 
 /**
- * 
+ * PolyphonicMusicPlayer
+ * Plays two or more audio files simultaneously to create polyphonic music
+ * Supports either a melody and a harmony (homophonic music), or the same melody played multiple times with an offset (polyphonic music)
  */
 public class PolyphonicMusicPlayer {
 	
-	private long delay;
-	private int totalCount;
-	private String melodyFileString;
-	private String harmonyFileString;
-	private boolean polyCheck = false;
+	private long delay; //delay in seconds between playing the multiple audio files
+	private int totalCount; //total number of audio files
+	private String melodyFileString; //name of the audio file that is the melody
+	private String harmonyFileString; //name of the audio file that is the harmony
+	private boolean polyCheck = false; //boolean to check if the music is polyphonic or homophonic
 	
-	
+	/**
+	 * Constructor for if the music to be played is one melody played multiple times with an offset
+	 * @param delay delay in seconds between the start of each melody being played
+	 * @param totalCount total number of melodies to play
+	 * @param audioFileString name of the audio file to which we play music from
+	 */
 	public PolyphonicMusicPlayer(long delay, int totalCount, String audioFileString) {
-		this.delay = delay;
+		this.delay = delay; //assign parameters to the given values in the constructor
 		this.totalCount = totalCount;
 		this.melodyFileString = audioFileString;
-		this.polyCheck = true;
+		this.polyCheck = true; //flip polyphonic check to true, music is polyphonic
 	}
 	
+	/**
+	 * Constructor to play homophonic music, two audio files are given, a melody and a harmony to support it
+	 * @param delay delay in seconds between the start of the melody and start of the harmony
+	 * @param melodyFileString name of the melody audio file
+	 * @param harmonyFileString name of the harmony audio file
+	 */
 	public PolyphonicMusicPlayer(long delay, String melodyFileString, String harmonyFileString) {
-		this.delay = delay;
-		this.totalCount = 2;
-		this.melodyFileString = melodyFileString;
+		this.delay = delay; //delay parameter is set to given delay
+		this.totalCount = 2; //total count is set to 2 as there are always 2 audio files in this constructor
+		this.melodyFileString = melodyFileString; //melody and harmony are set
 		this.harmonyFileString = harmonyFileString;
 	}
 	
+	/**
+	 * Plays audio tracks at the same time using StdAudio to create audio threads that run in the background
+	 * Supports both polyphonic and homophonic music
+	 */
 	public void play() {
-		if(this.polyCheck) {
-			for(int i=0; i<totalCount; i++) {
+		if(this.polyCheck) { //Check if the music is polyphonic, if so
+			for(int i=0; i<totalCount; i++) { //for the total number of tracks, play each track in the background
 				StdAudio.playInBackground(melodyFileString);
 				try {
-					TimeUnit.SECONDS.sleep(delay);
+					TimeUnit.SECONDS.sleep(delay); //delay after starting each track
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
 			}
 		}
-		else {
-			StdAudio.playInBackground(melodyFileString);
+		else { //if the music is homophonic
+			StdAudio.playInBackground(melodyFileString); //play the melody in the background
 			try {
-				TimeUnit.SECONDS.sleep(delay);
+				TimeUnit.SECONDS.sleep(delay); //delay for the given delay
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			StdAudio.playInBackground(harmonyFileString);
+			StdAudio.playInBackground(harmonyFileString); //play the harmony in the background next
 		}
 		
 	}
+	
+	/**
+	 * Test the polyphonic music player by playing an audio track multiple times with a delay
+	 * 
+	 * @param args command line arguments
+	 */
 	public static void main(String[] args) {
-		PolyphonicMusicPlayer polyphonicMusicPlayer = new PolyphonicMusicPlayer(0, "frere.wav", "stairway.wav");
+		PolyphonicMusicPlayer polyphonicMusicPlayer = new PolyphonicMusicPlayer(8, 3, "frere.wav");
 		polyphonicMusicPlayer.play();
 	}
 }
