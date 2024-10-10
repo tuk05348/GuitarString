9a5c12c1167eec799e0dc95a8819db0a1506a248
Niaz Baharudeen <tuk05348@temple.edu>
2024-10-09 23:16:21 -0400

    Created a basic InstrumentHero Factory for creating new "Hero" objects based on instruments.

---
+++ src/main/java/work/niaz/example/GuitarString/InstrumentHeroFactory.java
@@ -0,0 +1,22 @@
+package work.niaz.example.GuitarString;
+
+
+public class InstrumentHeroFactory {
+	public InstrumentHero createInstrumentHero(String type) {
+		if(type.equalsIgnoreCase("guitar")) {
+			return new GuitarHero();
+		}
+		else if(type.equalsIgnoreCase("band")){
+			return new BandHero();
+		}
+		else if(type.equalsIgnoreCase("drum")) {
+			return new DrumHero();
+		}
+		else if(type.equalsIgnoreCase("harp")) {
+			return new HarpHero();
+		}
+		else {
+			throw new IllegalArgumentException();
+		}
+	}
+}
