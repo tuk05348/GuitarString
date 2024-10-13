c1c85a3a7fc74110b3840f40666d3a185cf7087c
Niaz Baharudeen <tuk05348@temple.edu>
2024-10-13 11:57:06 -0400

    Added BandHero and DrumHero which extend InstrumentHero for a mix of harps, guitars, and drums, and just the drums.

--- src/main/java/work/niaz/example/GuitarString/BandHero.java
+++ src/main/java/work/niaz/example/GuitarString/BandHero.java
@@ -1,11 +1,45 @@
 package work.niaz.example.GuitarString;
 
+import edu.princeton.cs.algs4.StdDraw;
+
 public class BandHero extends InstrumentHero {
+	
+	public BandHero() {
+		name = "Band";
+		keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
+		instruments = new Instrument[keyboard.length()];
+		for(int i=0; i<keyboard.length(); i++) {
+			if(i < 13 && i%3 == 0) {
+				instruments[i] = new Drum(Tuning.calculateFreq(i));
+			}
+			else if(i < 13 && i%3 != 0) {
+				instruments[i] = new GuitarString(Tuning.calculateFreq(i));
+			}
+			else if(i >= 13 && i < 25) {
+				instruments[i] = new GuitarString(Tuning.calculateFreq(i));
+			}
+			else {
+				instruments[i] = new HarpString(Tuning.calculateFreq(i));
+			}
+		}
+	}
 
 	@Override
 	void setUpVisualizer() {
-		// TODO Auto-generated method stub
-		
+		StdDraw.setTitle(name + " Hero");
+		StdDraw.setCanvasSize(1000, 400);
+		StdDraw.enableDoubleBuffering();	//enables real time animation
+		StdDraw.setPenRadius(0.001);
+        StdDraw.setPenColor(StdDraw.GREEN);
+        StdDraw.setXscale(0, 100);
+        StdDraw.setYscale(-0.5, 0.5);
+        StdDraw.point(0, 0);	//ensures a StdDraw window will always open when ran
+        StdDraw.text(50, -0.25, "Keyboard Mapping: " + keyboard);
+	}
+	
+	public static void main(String[] args) {
+		BandHero bandHero = new BandHero();
+		bandHero.startSession();
 	}
 
 }
--- src/main/java/work/niaz/example/GuitarString/DrumHero.java
+++ src/main/java/work/niaz/example/GuitarString/DrumHero.java
@@ -1,11 +1,34 @@
 package work.niaz.example.GuitarString;
 
+import edu.princeton.cs.algs4.StdDraw;
+
 public class DrumHero extends InstrumentHero {
 
+	public DrumHero() {
+		name = "Drum";
+		keyboard = "qw45yuio-=xc";
+		instruments = new Instrument[keyboard.length()];
+		for(int i=0; i < 2*keyboard.length(); i += 2) {
+			instruments[i/2] = new Drum(Tuning.calculateFreq(i));
+		}
+	}
+	
 	@Override
 	void setUpVisualizer() {
-		// TODO Auto-generated method stub
-		
+		StdDraw.setTitle(name + " Hero");
+		StdDraw.setCanvasSize(400, 400);
+		StdDraw.enableDoubleBuffering();	//enables real time animation
+		StdDraw.setPenRadius(0.001);
+        StdDraw.setPenColor(StdDraw.RED);
+        StdDraw.setXscale(0, 100);
+        StdDraw.setYscale(-0.5, 0.5);
+        StdDraw.point(0, 0);	//ensures a StdDraw window will always open when ran
+        StdDraw.text(50, -0.25, "Keyboard Mapping: " + keyboard);
+	}
+	
+	public static void main(String[] args) {
+		DrumHero drumHero = new DrumHero();
+		drumHero.startSession();
 	}
 
 }
