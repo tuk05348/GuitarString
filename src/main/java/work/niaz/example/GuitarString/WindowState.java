2ff5abefedb8c55cb1fddeba6667878e06d9452f
Niaz Baharudeen <tuk05348@temple.edu>
2024-10-16 22:26:56 -0400

    Added getters and setters in WindowState for the x scale and y scale.

--- src/main/java/work/niaz/example/GuitarString/WindowState.java
+++ src/main/java/work/niaz/example/GuitarString/WindowState.java
@@ -3,8 +3,12 @@ package work.niaz.example.GuitarString;
 public class WindowState {
 	
 	private String note;
+	private double xScale;
+	private double yScale;
 	
-	public WindowState(boolean noteCheck, String note) {
+	public WindowState(boolean noteCheck, String note, double xScale, double yScale) {
+		this.xScale = xScale;
+		this.yScale = yScale;
 		if(noteCheck) {
 			this.note = note;
 		}
@@ -15,5 +19,13 @@ public class WindowState {
 	
 	public String getNote() {
 		return note;
+	}
+	
+	public double getXScale() {
+		return xScale;
+	}
+	
+	public double getYScale() {
+		return yScale;
 	}
 }
